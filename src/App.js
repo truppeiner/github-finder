8503ff3343a17fe6fe8da2ccd454efb6977f5a9b
Thomas Ruppeiner <truppeiner@gmail.com>
2022-08-15 13:14:06 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
-// import './App.css';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 function App() {
   return (
+    <Router>
       <div>
-        <h1>Hello World!</h1>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar/>
+          <main>
+            Content
+          </main>
+        </div>
       </div>
+    </Router>
   );
 }
 
